a5ba10367001f7bc9cebfc26ec941cccb1200d39
ashe <445090260@qq.com>
2019-03-09 17:31:33 +0800

    swagger2

--- src/main/java/com/explore/controller/PaperController.java
+++ src/main/java/com/explore/controller/PaperController.java
@@ -6,6 +6,8 @@ import com.explore.pojo.PaperCompose;
 import com.explore.pojo.Question;
 import com.explore.service.IPaperService;
 import com.explore.service.IQuestionService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +15,7 @@ import java.util.List;
 
 
 @RestController
+@Api(value = "试卷库的接口")
 @RequestMapping("/paper")
 public class PaperController {
 
@@ -28,6 +31,7 @@ public class PaperController {
      * @return
      */
     @PostMapping
+    @ApiOperation(value="向试卷库新增试卷基本信息", notes="参数为一个Paper对象")
     public ServerResponse save(Paper paper){
         return paperService.savePaper(paper);
     }
@@ -37,6 +41,7 @@ public class PaperController {
      * @return
      */
     @GetMapping(value = "/{id}")
+    @ApiOperation(value="获得指定试卷基本信息", notes="通过id获得")
     public ServerResponse<Paper> getPaperById(@PathVariable("id")Integer id){
         return paperService.getPaperById(id);
     }
@@ -46,6 +51,7 @@ public class PaperController {
      * @return
      */
     @GetMapping
+    @ApiOperation(value="获得所有试卷基本信息", notes="不需要参数")
     public ServerResponse<List<Paper>> getAllPaper(){
         return paperService.getAllPaper();
     }
@@ -55,6 +61,7 @@ public class PaperController {
      * @param id
      */
     @DeleteMapping(value = "/{id}")
+    @ApiOperation(value="删除试卷", notes="通过id删除")
     public ServerResponse deletePaperByPaperId(@PathVariable("id")Integer id){
         return paperService.deletePaperByPaperId(id);
     }
@@ -64,6 +71,7 @@ public class PaperController {
      * @param id,paper
      */
     @PutMapping(value ="/{id}")
+    @ApiOperation(value="修改试卷基本信息", notes="通过id和一个Paper对象")
     public ServerResponse edit(@PathVariable("id")Integer id,Paper paper){
         return paperService.editPaperByPaperId(id,paper);
     }
@@ -74,6 +82,7 @@ public class PaperController {
      * @return
      */
     @GetMapping(value = "/details/{paperId}")
+    @ApiOperation(value="获得指定试卷的题目信息", notes="通过指定试卷的id")
     public ServerResponse<List<Question>> get(@PathVariable("paperId")Integer paperId){
         return paperService.getDetailsByPaperId(paperId);
     }
@@ -83,6 +92,7 @@ public class PaperController {
      * @param paperId,paperCompose
      */
     @PostMapping(value = "/details/{paperId}")
+    @ApiOperation(value="向指定试卷添加题目", notes="指定试卷的id和一个PaperCompose对象")
     public ServerResponse addPaperComposeByPaperId(@PathVariable("paperId")Integer paperId, PaperCompose paperCompose){
         return paperService.addPaperComposeByPaperId(paperId,paperCompose);
     }
@@ -92,6 +102,7 @@ public class PaperController {
      * @param paperId,paperCompose
      */
     @PutMapping(value = "/details/{paperId}")
+    @ApiOperation(value="向指定试卷修改题目", notes="指定试卷的id和一个PaperCompose对象")
         public ServerResponse editPaperComposeByPaperId(@PathVariable("paperId")Integer paperId, PaperCompose paperCompose){
         return paperService.editPaperComposeByPaperId(paperId,paperCompose);
     }
@@ -100,6 +111,7 @@ public class PaperController {
      * @param paperId,paperCompose
      */
     @DeleteMapping(value = "/details/{paperId}")
+    @ApiOperation(value="向指定试卷输出指定题目", notes="指定试卷的id和题目的序列号")
     public ServerResponse deletePaperComposeBySequenceAndPaperId(@PathVariable("paperId")Integer paperId, Integer sequence){
         return paperService.deletePaperComposeBySequenceAndPaperId(paperId,sequence);
     }
@@ -109,6 +121,7 @@ public class PaperController {
      * @param paperId,questionTypeId,quantity,singeScore,subjectId,keyPoint
      */
     @PostMapping(value = "/auto/{paperId}")
+    @ApiOperation(value="自动生成试卷的题目", notes="通过试卷id，问题类型id，生成的数量，每一个题目的成绩，科目id，知识点")
     public ServerResponse<List<Question>> autoQuestion(@PathVariable("paperId")Integer paperId,  Integer questionTypeId, Integer quantity, Double singeScore, Integer subjectId, String keyPoint){
         return paperService.autoQuestion(paperId,questionTypeId,quantity,singeScore,subjectId,keyPoint);
     }
--- src/main/java/com/explore/controller/QuestionController.java
+++ src/main/java/com/explore/controller/QuestionController.java
@@ -3,14 +3,16 @@ package com.explore.controller;
 import com.explore.common.ServerResponse;
 import com.explore.pojo.Question;
 import com.explore.service.IQuestionService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 
 @RestController
+@Api(value = "试题库的接口")
 @RequestMapping(value = "/question")
 public class QuestionController {
 
@@ -20,6 +22,7 @@ public class QuestionController {
      * 添加问题
      */
     @PostMapping
+    @ApiOperation(value="向试题库添加试题", notes="参数为一个Question对象")
     public ServerResponse save(Question question){
         return questionService.saveQuestion(question);
     }
@@ -27,21 +30,25 @@ public class QuestionController {
      * 修改问题
      */
     @PutMapping(value ="/{id}")
+    @ApiOperation(value="向试题库修改试题", notes="通过id和一个Question对象进行修改")
     public  ServerResponse edit(@PathVariable("id")Integer questionId,Question question){return  questionService.editQuestionByQuestionId(questionId,question);}
     /**
      * 删除试题
      */
     @DeleteMapping(value ="/{id}")
+    @ApiOperation(value="向试题库删除试题", notes="通过id删除试题")
     public ServerResponse delete(@PathVariable("id")Integer questionId){return  questionService.deleteQuestionByQuestionId(questionId);}
     /**
      * 获得指定问题
      */
     @GetMapping(value ="/{id}")
+    @ApiOperation(value="从试题库获得指定试题", notes="通过id获得")
     public  ServerResponse<Question> get(@PathVariable("id")Integer questionId){return  questionService.getQuestionsByQuestionId(questionId);}
     /**
      * 获得所有问题
      */
     @GetMapping
+    @ApiOperation(value="从试题库获得所有试题", notes="不需要参数")
     public  ServerResponse<List<Question>> get(){return  questionService.getAllQuestions();}
 
 
