8e6efe19b9445592ef984811431b1c304f0321df
egbiner2 <794409767@qq.com>
2019-04-09 20:37:29 +0800

    新增BatchStudent关系表

---
+++ src/main/java/com/explore/dao/BatchStudentMapper.java
@@ -0,0 +1,17 @@
+package com.explore.dao;
+
+import com.explore.pojo.BatchStudent;
+
+public interface BatchStudentMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(BatchStudent record);
+
+    int insertSelective(BatchStudent record);
+
+    BatchStudent selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(BatchStudent record);
+
+    int updateByPrimaryKey(BatchStudent record);
+}
---
+++ src/main/java/com/explore/pojo/BatchStudent.java
@@ -0,0 +1,95 @@
+package com.explore.pojo;
+
+import java.util.Date;
+
+public class BatchStudent {
+    private Integer id;
+
+    private Integer batchId;
+
+    private Integer studentId;
+
+    private Double score;
+
+    private Integer status;
+
+    private Date startTime;
+
+    private Date submitTime;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getBatchId() {
+        return batchId;
+    }
+
+    public void setBatchId(Integer batchId) {
+        this.batchId = batchId;
+    }
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Integer studentId) {
+        this.studentId = studentId;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getSubmitTime() {
+        return submitTime;
+    }
+
+    public void setSubmitTime(Date submitTime) {
+        this.submitTime = submitTime;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
